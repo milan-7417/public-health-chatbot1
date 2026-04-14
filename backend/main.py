7930de7cd2602fd03ff1619749d5fe1e52cf52b6
milan-7417 <mrai48851@gmail.com>
2026-04-14 13:04:26 +0530

    initial commit

--- backend/main.py
+++ backend/main.py
@@ -20,19 +20,16 @@ load_dotenv()
 uploaded_report_text = ""
 
 app = FastAPI()
-import sys
-import os
 
-def resource_path(relative_path):
-    if hasattr(sys, '_MEIPASS'):
-        return os.path.join(sys._MEIPASS, relative_path)
-    return os.path.join(os.path.abspath("."), relative_path)
 
 # =========================
 # ✅ STATIC FILES
 # =========================
-app.mount("/", StaticFiles(directory=resource_path("frontend"), html=True), name="frontend")
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
 
+@app.get("/")
+def home():
+    return FileResponse("frontend/index.html")
 
 # =========================
 # ✅ CORS
